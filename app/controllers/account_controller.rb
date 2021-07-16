528e2632fa363ef0ed93c7ffac8848a61027756e
Rommel Torquator <rommeltorquator09@gmail.com>
2021-07-16 00:56:13 -0400

    Fixed some rubocop generated errors

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -8,10 +8,11 @@ class AccountController < ApplicationController
 
     @new_user = User.new(
       email: params[:user][:email],
-      first_name: params[:user][:first_name], 
-      last_name: params[:user][:last_name], 
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
       password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation])
+      password_confirmation: params[:user][:password_confirmation]
+    )
 
     @new_user.type = 'Member'
     @new_user.team_id = current_admin.team_id
@@ -20,7 +21,7 @@ class AccountController < ApplicationController
     if @new_user.save
       redirect_to root_path, notice: 'Procedure was successfully created.'
     else
-      render "new"
+      render 'new'
     end
   end
 end
