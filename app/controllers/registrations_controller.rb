4bd859ad728b680daa17246cdcd76c17bcd13d97
Rommel Torquator <rommeltorquator09@gmail.com>
2021-07-19 07:30:33 -0400

    Implemented the mailer feature

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-  def after_update_path_for(resource)
-      # puts 'this is happening yoyo mama'
-      # flash[:notice] = "Account succesfully updated"
-      home_dashboard_path
+
+  def after_update_path_for(_resource)
+    # puts 'this is happening yoyo mama'
+    # flash[:notice] = "Account succesfully updated"
+    home_dashboard_path
+  end
+
+  def after_inactive_sign_up_path_for(_resource)
+    flash[:notice] = "Account has been successfully created."
+    home_dashboard_path
   end
 end
